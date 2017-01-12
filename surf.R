deb7d8844f65ab034f3e63746ba3c18c39b2c01c
Gordon Maclean <maclean@users.noreply.github.com>
2017-01-12 11:29:00 -0700

    More cleanup

--- surf.R
+++ surf.R
@@ -1,3 +1,4 @@
+# vim: set expandtab autoindent ts=4 sw=4:
 
 # Read and plot signal quality data that has been extracted
 # by surfboard.py from a Surfboard SB6190 cable modem.
@@ -7,72 +8,81 @@
 
 surfboard <- function(file=file.path(Sys.getenv("HOME"),"surfboard.dat.gz"))
 {
-    x <- scan(file=file,sep=",", quiet=TRUE, what=list(
+    surfd <- scan(file=file,sep=",", quiet=TRUE, what=list(
             datetime="", channel=1, status="",
             modulation="", id=1, freq=1.0, power=1.0,
             SNR=1.0, CorrCw=1, UncorrCw=1))
 
-    times <- utime(x$datetime,in.format="%Y-%m-%d %H:%M:%S")
-    uchans <- unique(x$channel)
+    times <- utime(surfd$datetime,in.format="%Y-%m-%d %H:%M:%S")
+
+    # The mapping from channel number to channel id has changed from time to time.
+    # It appears that the ids are mapped one-to-one to frequencies,
+    # so we'll use the id as the main key
+    uids <- sort(unique(surfd$id))
 
     tsl <- list()
 
-    for (chan in uchans) {
-        ic <- x$channel == chan
-        dx <- c(x$power[ic],x$SNR[ic],
-                x$CorrCw[ic], x$UncorrCw[ic])
-        tx <- times[ic]
+    for (id in uids) {
+        mx <- surfd$id == id
+        dx <- c(surfd$power[mx],surfd$SNR[mx],
+                surfd$CorrCw[mx], surfd$UncorrCw[mx])
+        tx <- times[mx]
         xts <- dat(nts(matrix(dx,ncol=4), tx,
             names=c("power","SNR","CorrCw","UncorrCw"),
             units=c("dBmV","dB","","")))
 
-        tsl[chan] <- list(list(ts=xts,freq=unique(x$freq[ic]),
-		id=unique(x$id[ic]),frequnits="MHz"))
+        tsl[id] <- list(list(ts=xts, freq=unique(surfd$freq[mx]),
+            channels=unique(surfd$channel[mx]), frequnits="MHz"))
     }
     tsl
 }
 
-plotsurf <- function(channels=NULL)
+plotsurf <- function(ids=NULL)
 {
-    Sys.setenv(PROJECT="")
-    x <- surfboard()
-    par(mfrow=c(2,2),ask=TRUE)
-    nc <- length(x)
+    surfd <- surfboard()
+    nid <- length(surfd)
 
     alltoo <- FALSE
-    if (is.null(channels)) {
-        channels <- 1:nc
+    if (is.null(ids)) {
+        ids <- 1:nid
         alltoo <- TRUE
     }
     else {
-        ac <- !is.na(match(channels, 0))
+        ac <- !is.na(match(ids, 0))
         if (any(ac)) {
-            channels <- channels[!ac]
+            ids <- ids[!ac]
             alltoo <- TRUE
         }
     }
+
+    Sys.setenv(PROJECT="")
+    ask <-  (alltoo + length(ids)) > 1
+    par(mfrow=c(2,2))
        
-    nval <- NULL
     corr <- NULL
     uncorr <- NULL
     snr <- NULL
     pow <- NULL
-    for (ic in 1:nc) {
 
-        corx <- d_by_dt(x[[ic]]$ts[-1,"CorrCw"],dtmax=86400,lag=1,time=1) * 3600
-	units(corx) <- "hr^-1"
+    for (dx in surfd) {
 
-        uncorx <- d_by_dt(x[[ic]]$ts[-1,"UncorrCw"],dtmax=86400,lag=1,time=1) * 3600
-	units(uncorx) <- "hr^-1"
+        # This modem does not report the number of codewords that didn't need correcting,
+        # just the number of corrected and the unncorrectables. So we don't actually
+        # know the number of successful or failed corrections as a percentage of the
+        # total number of codewords.
 
-        nvalx <- corx + uncorx
-        colnames(nvalx) <- "CodeWords"
-        nval <- Cbind(nval,nvalx)
+        # rate (per hour) of codewords that were corrected
+        corx <- d_by_dt(dx$ts[,"CorrCw"],dtmax=86400,lag=1,time=1) * 3600
+        units(corx) <- "hr-1"
         corr <- Cbind(corr,corx)
+
+        # rate (per hour) of uncorrectable codewords
+        uncorx <- d_by_dt(dx$ts[,"UncorrCw"],dtmax=86400,lag=1,time=1) * 3600
+        units(uncorx) <- "hr-1"
         uncorr <- Cbind(uncorr,uncorx)
-        
-        snr <- Cbind(snr, x[[ic]]$ts[,"SNR"])
-        pow <- Cbind(pow, x[[ic]]$ts[,"power"])
+
+        snr <- Cbind(snr, dx$ts[,"SNR"])
+        pow <- Cbind(pow, dx$ts[,"power"])
     }
     # look for modem restarts when the successive difference of
     # correctables or uncorrectables are negative.
@@ -82,54 +92,60 @@ plotsurf <- function(channels=NULL)
             positions(uncorr),names="restarts",units=""))
 
     if (any(restart)) {
-        nval[restart,] <- NA
+        corr[restart,] <- NA
         uncorr[restart,] <- NA
     }
         
-    t1 <- start(x[[1]]$ts)
-    # t1 <- utime("2017 jan 2 05:00")
-    t2 <- end(x[[1]]$ts)
+    # total rate of codewords that needed correction
+    badcw <- corr + uncorr
+    colnames(badcw) <- rep("BadCodeWords", ncol(badcw))
+    units(badcw) <- rep("hr-1", ncol(badcw))
 
-    clip("CodeWords", 0.5, 1.e100)
+    t1 <- start(snr)
+    # t1 <- utime("2017 jan 2 05:00")
+    t2 <- end(snr)
+
+    clip("BadCodeWords", 0.1, 1.e100)
     clip("UncorrCW", 0, 100)
 
-    for (ic in channels) {
+    for (id in ids) {
 
-        pos  <- !is.na(nval[,ic]) & nval[,ic]
+        freq <- surfd[[id]]$freq
+        funits <- surfd[[id]]$frequnits
+        chans <- surfd[[id]]$channels
+        titlestr <- paste0("Channel id=",id,
+            ", freq=",freq," ",funits, ", chan #s=",
+            paste(chans, collapse=","))
 
-        if (any(pos)) {
+        zcw  <- !is.na(badcw[,id]) & badcw[,id] == 0
+        badcw[zcw,id] <- 0.1
+        plot(badcw[,id], title=titlestr, type="b", xlim=c(t1,t2), log="y")
+        badcw[zcw,id] <- 0
+        par(ask=ask)
 
-            freq <- x[[ic]]$freq
-            funits <- x[[ic]]$frequnits
-            id <- x[[ic]]$id
-            titlestr <- paste0("Channel ",ic, ", id=",id,
-                ", freq=",freq," ",funits)
+        pcu <- uncorr[,id] / badcw[,id] * 100
+        colnames(pcu) <- "UncorrCW"
+        units(pcu) <- "%"
+        plot(pcu,title=titlestr,type="b",xlim=c(t1,t2))
 
-
-            plot(nval[pos,ic],title=titlestr,type="b",xlim=c(t1,t2),log="y")
-
-            pcu <- uncorr[pos,ic] / nval[pos,ic] * 100
-	    colnames(pcu) <- "UncorrCW"
-            units(pcu) <- "%"
-            plot(pcu,title=titlestr,type="b",xlim=c(t1,t2))
-
-            plot(snr[,ic],title=titlestr,type="b",xlim=c(t1,t2))
-            plot(pow[,ic],title=titlestr,type="b",xlim=c(t1,t2))
-        }
+        plot(snr[,id],title=titlestr,type="b",xlim=c(t1,t2))
+        plot(pow[,id],title=titlestr,type="b",xlim=c(t1,t2))
     }
 
     if (alltoo) {
-        # Plot total and error %age across all channels
+        # Plot total and error %age across all ids
         titlestr <- "All channels"
 
-        nval[, 1] <- apply(nval@data, 1, function(x) { sum(x, na.rm=TRUE) })
+        badcw[, 1] <- apply(badcw@data, 1, function(x) { sum(x, na.rm=TRUE) })
         uncorr[, 1] <- apply(uncorr@data, 1, function(x) { sum(x, na.rm=TRUE) })
 
-        # pos  <- nval[,1] > 0
+        # make log plot to expand lower values. Plot 0 as 0.1
+        zcw  <- !is.na(badcw[,1]) & badcw[,1] == 0
+        badcw[zcw,1] <- 0.1
+        plot(badcw[,1], title=titlestr,type="b",xlim=c(t1,t2),log="y")
+        badcw[zcw,1] <- 0
 
-        plot(nval[,1], title=titlestr,type="b",xlim=c(t1,t2),log="y")
-
-        pcu <- uncorr[,1] / nval[,1] * 100
+        pcu <- uncorr[,1] / badcw[,1] * 100
         colnames(pcu) <- "UncorrCW"
         units(pcu) <- "%"
         plot(pcu,title=titlestr,type="b",xlim=c(t1,t2))
