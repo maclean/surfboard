dd8de8989707f3aa0df62bd603dab2f5dee76f33
Gordon Maclean <maclean@users.noreply.github.com>
2017-01-18 23:23:22 -0700

    Still twiddling with launchctl commmands

--- launch.sh
+++ launch.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
 sudo cp surfboard.plist /Library/LaunchDaemons
-launchctl unload /Library/LaunchDaemons/surfboard.plist
-launchctl load /Library/LaunchDaemons/surfboard.plist
-# launchctl start local.surfboard
+if false; then
+    launchctl unload /Library/LaunchDaemons/surfboard.plist
+    launchctl load /Library/LaunchDaemons/surfboard.plist
+    launchctl start local.surfboard
+else
+
+    # not sure bootstrap is needed
+    sudo launchctl bootstrap user/501/local.surfboard \
+	/Library/LaunchDaemons/surfboard.plist
+
+    launchctl enable user/501/local.surfboard
+    launchctl load /Library/LaunchDaemons/surfboard.plist
+
+    launchctl list | fgrep surfboard
+    launchctl print gui/501/local.surfboard
+fi
+
