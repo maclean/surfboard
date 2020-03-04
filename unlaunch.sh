16e5707568f8b84c6fa974d1b6046dec3d5d7187
Gordon Maclean <maclean@users.noreply.github.com>
2020-03-04 16:14:34 -0700

    fix

--- unlaunch.sh
+++ unlaunch.sh
@@ -4,5 +4,5 @@ sudo launchctl list | fgrep surfboard
 sudo launchctl stop local.surfboard
 sudo launchctl remove local.surfboard
 sudo launchctl list | fgrep surfboard
-sudo rm /Library/LaunchAgents/surfboard.plist
+sudo rm -f /Library/LaunchAgents/surfboard.plist
 
