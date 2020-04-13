585e81b64f116734c8a09991c726a82b777d6417
Gordon Maclean <maclean@users.noreply.github.com>
2020-04-12 22:17:58 -0600

    Plot updates

--- surf.R
+++ surf.R
@@ -6,7 +6,7 @@
 # at http://www.eol.ucar.edu/software/R/, for manipulating
 # and plotting time-series.
 
-surfboard <- function(file=file.path(Sys.getenv("HOME"),"surfboard","surfboard.dat.gz"), modemtz="MST")
+surfboard <- function(file=file.path(Sys.getenv("HOME"),"surfboard","surfboard.dat.gz"), modemtz="MST", do_dat=FALSE)
 {
     surfd <- scan(file=file,sep=",", quiet=TRUE, what=list(
             datetime="", channel=1, status="",
@@ -28,9 +28,10 @@ surfboard <- function(file=file.path(Sys.getenv("HOME"),"surfboard","surfboard.d
         dx <- c(surfd$power[mx],surfd$SNR[mx],
                 surfd$CorrCw[mx], surfd$UncorrCw[mx])
         tx <- times[mx]
-        xts <- dat(nts(matrix(dx,ncol=4), tx,
+        xts <- nts(matrix(dx,ncol=4), tx,
             names=c("power","SNR","CorrCw","UncorrCw"),
-            units=c("dBmV","dB","","")))
+            units=c("dBmV","dB","",""))
+        if (do_dat) xts <- dat(xts)
 
         tsl[as.character(freq)] <- list(list(ts=xts,
             channels=unique(surfd$channel[mx]),
@@ -42,11 +43,11 @@ surfboard <- function(file=file.path(Sys.getenv("HOME"),"surfboard","surfboard.d
 
 plotsurf <- function(freqs=0,
     file=file.path(Sys.getenv("HOME"),"surfboard","surfboard.dat.gz"),
-    palate="Heat", ncolors=10, modemtz="MST")
+    palate="Heat", ncolors=10, modemtz="MST", logbad=FALSE, do_dat=FALSE)
 {
     # not really important, but time on the modem is standard time,
     # not adjusted for daylight savings time
-    surfd <- surfboard(file=file, modemtz=modemtz)
+    surfd <- surfboard(file=file, modemtz=modemtz, do_dat=do_dat)
 
     allfreqs <- sort(as.integer(names(surfd)))
     cat("Frequencies=",paste(allfreqs,collapse=", "),"\n")
@@ -84,7 +85,6 @@ plotsurf <- function(freqs=0,
     snr <- NULL
     pow <- NULL
 
-    logbad <- FALSE
 
     for (freq in calcfreqs) {
         dx <- surfd[[as.character(freq)]]
@@ -95,12 +95,15 @@ plotsurf <- function(freqs=0,
         # total number of codewords.
 
         # rate (per hour) of codewords that were corrected
+        # cat('class(dx$ts[,"CorrCw"]=', class(dx$ts[,"CorrCw"]),"\n")
         corx <- d_by_dt(dx$ts[,"CorrCw"],dtmax=86400,lag=1,time=1) * 3600
+        # cat("class(corx)=", class(corx), "\n")
         units(corx) <- "hr-1"
         corr <- Cbind(corr,corx)
 
         # rate (per hour) of uncorrectable codewords
         uncorx <- d_by_dt(dx$ts[,"UncorrCw"],dtmax=86400,lag=1,time=1) * 3600
+        # cat("class(uncorx)=", class(uncorx), "\n")
         units(uncorx) <- "hr-1"
         uncorr <- Cbind(uncorr,uncorx)
 
@@ -109,10 +112,10 @@ plotsurf <- function(freqs=0,
     }
     # look for modem restarts when the successive difference of
     # correctables or uncorrectables are negative.
-    restart <- dat(nts(apply(corr@data,1,function(x) { any(!is.na(x) & x < 0)}),
-            positions(corr),names="restarts",units="")) |
-        dat(nts(apply(uncorr@data,1,function(x) { any(!is.na(x) & x < 0)}),
-            positions(uncorr),names="restarts",units=""))
+    restart <- nts(apply(corr@data,1,function(x) { any(!is.na(x) & x < 0)}),
+            positions(corr),names="restarts",units="") |
+        nts(apply(uncorr@data,1,function(x) { any(!is.na(x) & x < 0)}),
+            positions(uncorr),names="restarts",units="")
 
     if (any(restart)) {
         corr[restart,] <- NA
@@ -143,26 +146,53 @@ plotsurf <- function(freqs=0,
             ", ids=", paste(ids,collapse=","),
             ", channel#=", paste(chans,collapse=","))
 
+        zcw  <- !is.na(badcw[,nfreq]) & badcw[,nfreq] == 0
         if (logbad) {
             # make log plot to expand lower values. Plot 0 as 0.1
-            zcw  <- !is.na(badcw[,nfreq]) & badcw[,nfreq] == 0
             badcw[zcw,nfreq] <- 0.1
         }
-        plot(badcw[,nfreq], title=titlestr, type="b", xlim=c(t1,t2),
+        plot(badcw[,nfreq], type="b", xlim=c(t1,t2),
             log=ifelse(logbad,"y",""))
+        if (!do_dat) {
+            timeaxis(3, labels=TRUE, time.zone=badcw@time.zone, date.too=FALSE,
+                xlab=FALSE)
+            axis(4)
+        }
+
         if (logbad) badcw[zcw,nfreq] <- 0
 
         pcu <- uncorr[,nfreq] / badcw[,nfreq] * 100
         pcu[zcw,] <- 0
         colnames(pcu) <- "UncorrCW"
         units(pcu) <- "%"
-        plot(pcu,title=titlestr,type="b",xlim=c(t1,t2))
+        plot(pcu,type="b",xlim=c(t1,t2))
+        if (!do_dat) {
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE,
+                xlab=FALSE)
+            axis(4)
+        }
 
-        plot(snr[,nfreq],title=titlestr,type="b",xlim=c(t1,t2))
-        plot(pow[,nfreq],title=titlestr,type="b",xlim=c(t1,t2))
+        plot(snr[,nfreq],type="b",xlim=c(t1,t2))
+        if (!do_dat) {
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE,
+                xlab=FALSE)
+            axis(4)
+        }
+
+        if (!do_dat) {
+            plot(pow[,nfreq],type="b",xlim=c(t1,t2))
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE,
+                xlab=FALSE)
+            axis(4)
+            title(main=titlestr, line=-par("cex.main"), outer=TRUE)
+        }
+        else plot(pow[,nfreq],type="b",xlim=c(t1,t2), title=titlestr)
+
 
         par(ask=ask)
     }
+
+    eolts::set_plot_margins()
 
     # heatmaps of x=time, y=frequency, z=variable.
     # Need legends, time scale on X
@@ -179,31 +209,47 @@ plotsurf <- function(freqs=0,
     t2 <- tx[length(tx)]
 
     timeaxis_setup(t1,t2)
+    # par(mgp=c(1.7,0.7,0))
 
     title <- paste0(unique(colnames(badcw)), " (", unique(units(badcw)),")")
-    image(z=badcw@data, x=tx-t1, y=allfreqs, col=colors, ylab="MHz",
-        main=title, xaxt="n", xlab="")
-    timeaxis(1, time.zone=badcw@time.zone)
-    timeaxis(3, labels=FALSE, time.zone=badcw@time.zone)
+    image(z=badcw@data, x=tx-t1, y=allfreqs, col=colors,
+        ylab="MHz", xaxt="n", xlab="")
+    timeaxis(1, labels=FALSE, time.zone=badcw@time.zone)
+    timeaxis(3, labels=TRUE, time.zone=badcw@time.zone, date.too=FALSE,
+        xlab=FALSE)
+    axis(side=4)
+    mtext(title, side=3, line=1.5, cex=0.8)
 
     title <- paste0(unique(colnames(pcu)), " (", unique(units(pcu)),")")
-    image(z=pcu@data, x=tx-t1, y=allfreqs, zlim=c(0,100), col=colors, ylab="MHz", main=title, xaxt="n", xlab="")
-    timeaxis(1, time.zone=badcw@time.zone)
+    set_plot_margins()
+    image(z=pcu@data, x=tx-t1, y=allfreqs, zlim=c(0,100), col=colors,
+        ylab="MHz", xaxt="n", xlab="")
+    timeaxis(1, labels=FALSE, time.zone=badcw@time.zone)
     timeaxis(3, labels=FALSE, time.zone=badcw@time.zone)
+    axis(side=4)
+    mtext(title, side=3, line=0.5, cex=0.8)
 
     tx <- positions(snr)
     t1 <- tx[1]
     t2 <- tx[length(tx)]
     tscale <- t2 - t1
     title <- paste0(unique(colnames(snr)), " (", unique(units(snr)),")")
-    image(z=snr@data, x=tx - t1, y=allfreqs, col=colors, ylab="MHz", main=title, xaxt="n", xlab="")
-    timeaxis(1, time.zone=badcw@time.zone, date.too=TRUE)
+    set_plot_margins()
+    image(z=snr@data, x=tx - t1, y=allfreqs, col=colors,
+        ylab="MHz", xaxt="n", xlab="")
+    timeaxis(1, labels=FALSE, time.zone=badcw@time.zone)
     timeaxis(3, labels=FALSE, time.zone=badcw@time.zone)
+    axis(side=4)
+    mtext(title, side=3, line=0.5, cex=0.8)
 
     title <- paste0(unique(colnames(pow)), " (", unique(units(pow)),")")
-    image(z=pow@data, x=tx - t1, y=allfreqs, col=colors, ylab="MHz", main=title, xaxt="n", xlab="")
+    set_plot_margins()
+    image(z=pow@data, x=tx - t1, y=allfreqs, col=colors,
+        ylab="MHz", xaxt="n", xlab="")
     timeaxis(1, time.zone=badcw@time.zone, date.too=TRUE)
     timeaxis(3, labels=FALSE, time.zone=badcw@time.zone)
+    axis(side=4)
+    mtext(title, side=3, line=0.5, cex=0.8)
 
     # par(ask=ask)
 
@@ -218,12 +264,12 @@ plotsurf <- function(freqs=0,
         plot_ly(z=t(pcu@data), x=tx, y=allfreqs, type="heatmap")
         tx <- (as.numeric(positions(pow)) - t1) / 86400
         plot_ly(z=t(pow@data), x=tx, y=allfreqs, type="heatmap")
-         plot_ly(z=t(snr@data), x=tx, y=allfreqs, type="heatmap")
+        plot_ly(z=t(snr@data), x=tx, y=allfreqs, type="heatmap")
     }
 
     if (alltoo) {
         # Plot total and error %age across all frequencies
-        titlestr <- "All frequencies"
+        titlestr <- NULL
 
         badcw[, 1] <- apply(badcw@data, 1, function(x) { sum(x, na.rm=TRUE) })
         uncorr[, 1] <- apply(uncorr@data, 1, function(x) { sum(x, na.rm=TRUE) })
@@ -233,8 +279,12 @@ plotsurf <- function(freqs=0,
             zcw  <- !is.na(badcw[,1]) & badcw[,1] == 0
             badcw[zcw,1] <- 0.1
         }
-        plot(badcw[,1], title=titlestr,type="b",xlim=c(t1,t2),
+        plot(badcw[,1], type="b",xlim=c(t1,t2),
             log=ifelse(logbad,"y",""))
+        if (!do_dat) {
+            timeaxis(3, time.zone=badcw@time.zone, date.too=FALSE, xlab=FALSE)
+            axis(side=4)
+        }
         if (logbad) badcw[zcw,1] <- 0
 
         pcu <- uncorr[,1] / badcw[,1] * 100
@@ -242,12 +292,28 @@ plotsurf <- function(freqs=0,
         pcu[zcw,] <- 0
         colnames(pcu) <- "UncorrCW"
         units(pcu) <- "%"
-        plot(pcu,title=titlestr,type="b",xlim=c(t1,t2))
+        plot(pcu, type="b",xlim=c(t1,t2))
+        if (!do_dat) {
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE)
+            axis(side=4)
+        }
 
         snr@data[,1] <- apply(snr, 1, function(x) { mean(x, na.rm=T) })
+
+        plot(snr[,1], type="b", xlim=c(t1,t2))
+        if (!do_dat) {
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE)
+            axis(side=4)
+        }
+
         pow@data[,1] <- apply(pow, 1, function(x) { mean(x, na.rm=T) })
 
-        plot(snr[,1], title=titlestr, type="b", xlim=c(t1,t2))
-        plot(pow[,1], title=titlestr, type="b", xlim=c(t1,t2))
+        titlestr <- "All frequencies"
+        plot(pow[,1], type="b", xlim=c(t1,t2))
+        if (!do_dat) {
+            timeaxis(3, labels=FALSE, time.zone=badcw@time.zone, date.too=FALSE)
+            axis(side=4)
+            title(main=titlestr, line=-par("cex.main"), outer=TRUE)
+        }
     }
 }
