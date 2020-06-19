f6001c0f2b5339d3ce9caf462e77332e5f4813f6
Gordon Maclean <maclean@users.noreply.github.com>
2020-06-19 08:44:34 -0600

    Substitute environment variables in plists

--- launch.sh
+++ launch.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 whichplist"
+    echo "Eg: $0 surfboard"
+    exit 1
+fi
+
 which=$1
 
-sudo cp $which.plist /Library/LaunchDaemons
+sed -e s/\$USER/$USER/ -e s,\$HOME,$HOME, $which.plist > /tmp/$which.plist
+
+sudo cp /tmp/$which.plist /Library/LaunchDaemons
 
 sudo launchctl load /Library/LaunchDaemons/$which.plist
 
